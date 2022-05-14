638c64e5671bd682f20577cdcc326a619bcd13c0
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-05-14 12:54:44 +0200

    updates-Welcome.js-App.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Age } from "./Age";
 
 export class Welcome extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome, {<strong>{this.props.name}</strong>}</p>
-                {this.props.renderAge && <Age age={23}/>}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>Welcome, {<strong>{this.props.name}</strong>}</p>
+        {this.props.renderAge > 18 && this.props.renderAge < 65 && (
+          <Age age={this.props.renderAge} />
+        )}
+      </div>
+    );
+  }
 }
 
 Welcome.defaultProps = {
-    name: "Jonny"
-}
+  name: "Jonny",
+};
