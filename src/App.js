9bf60d1edff72a7fdafb97ceb1960f67047f9c09
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-05-23 19:02:49 +0200

    add-language-files

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Container } from "./Container";
 import { Counter } from "./Counter";
+import { DisplayLanguage } from "./DisplayLanguage";
 import { Hello } from "./Hello";
 import { InteractiveWelcome } from "./InteractiveWelcome";
+import { LanguageContext } from "./LanguageContext";
 import { Login } from "./Login";
 import { TodoList } from "./TodoList";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
+    state = {
+        language: "en"
+    }
+
+    handleLanguageChange = (event) => {
+        this.setState({
+            language: event.target.value
+        })
+    }
     render() {
         return (
             <div>
+                <LanguageContext.Provider value={this.state.language}>
+                    <select value={this.state.language} onChange={this.handleLanguageChange}>
+                        <option value="en">English</option>
+                        <option value="it">Italian</option>
+                    </select>
+                    <DisplayLanguage />
+                </LanguageContext.Provider>
                 <Hello />
                 <Welcome name="John" renderAge={true}/>
                 <Counter initialValue={2} incrementAmount={2} incrementInterval={2000} />
@@ -26,7 +44,7 @@ export class App extends React.Component {
                     (items) => {
                         const [...item] = items
                         return (
-                            <div>{item}</div>
+                            <ul><li>{item}</li></ul>
                         )
                     }
                 }/>
