bb44cb50a89bcc138eca8e5144a24e83ea296c31
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-05-24 10:58:42 +0200

    update-ClickCounter.js

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class ClickCounter extends React.Component {
+export function ClickCounter({ initialValue = 0 }) {
+  const [counter, setCounter] = useState(initialValue);
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
+  }
+  return (
+    <button onClick={handleCounterIncrement}>
+      Counter: {counter}
+    </button>
+  );
+}
+
+/*export class ClickCounter extends React.Component {
     state = {
         count: 0
     }
@@ -20,4 +32,4 @@ export class ClickCounter extends React.Component {
             </button>
         )
     }
-}
+}*/
