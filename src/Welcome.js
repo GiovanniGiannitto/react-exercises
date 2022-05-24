59e58bf9a9ad64beee1b04983117e4f803ffbad2
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-05-24 09:05:10 +0200

    update-Welcome.js-createFunction

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component {
+
+export function Welcome(props) {
+  return (
+    <div className="welcome">
+      <p>Welcome, {<strong>{props.name}</strong>}</p>
+      {props.renderAge && <Age age={23} />}
+    </div>
+    
+  )
+}
+
+/*export class Welcome extends React.Component {
   render() {
     return (
       <div className="welcome">
@@ -10,7 +21,7 @@ export class Welcome extends React.Component {
       </div>
     );
   }
-}
+}*/
 
 Welcome.defaultProps = {
   name: "Jonny",
