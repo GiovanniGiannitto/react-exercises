7abbd4bbb0490b249120d405139a6a911cf07b2c
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-06-08 18:47:21 +0200

    update-DisplayLanguage.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { CarDetail } from "./CarDetail";
+// import { CarDetail } from "./CarDetail";
 // import { ClickCounter } from "./ClickCounter";
 // import { ClickTracker } from "./ClickTracker";
 // import { Container } from "./Container";
 // import { Counter } from "./Counter";
-// import { DisplayLanguage } from "./DisplayLanguage";
-import { GitHubUser } from "./GitHubUser";
-import { GitHubUserList } from "./GitHubUserList";
+import { DisplayLanguage } from "./DisplayLanguage";
+// import { GitHubUser } from "./GitHubUser";
+// import { GitHubUserList } from "./GitHubUserList";
 // import { Hello } from "./Hello";
 // import { InteractiveWelcome } from "./InteractiveWelcome";
-// import { LanguageContext } from "./LanguageContext";
+import { LanguageContext } from "./LanguageContext";
 // import { Login } from "./Login";
 // import { Sum } from "./Sum";
 // import { TodoList } from "./TodoList";
 // import { UncontrolledLogin } from "./UncontrolledLogin";
 // import { Welcome } from "./Welcome";
-import { HookCounter } from "./HookCounter"
-import { HookForm } from "./HookForm";
+// import { HookCounter } from "./HookCounter"
+// import { HookForm } from "./HookForm";
 
 export class App extends React.Component {
     state = {
@@ -31,13 +31,7 @@ export class App extends React.Component {
     render() {
         return (
             <div>
-                {/* <LanguageContext.Provider value={this.state.language}>
-                    <select value={this.state.language} onChange={this.handleLanguageChange}>
-                        <option value="en">English</option>
-                        <option value="it">Italian</option>
-                    </select>
-                    <DisplayLanguage />
-                </LanguageContext.Provider>
+                {/* 
                 <Hello />
                 <Welcome name="John" renderAge={true}/>
                 <Counter initialValue={2} incrementAmount={2} incrementInterval={10000} />
@@ -56,11 +50,18 @@ export class App extends React.Component {
                 }/>
                 <Container title="My prop is a title" />
                 <Sum /> */}
-                <GitHubUser username="GiovanniGiannitto"/>
+                <LanguageContext.Provider value={this.state.language}>
+                    <select value={this.state.language} onChange={this.handleLanguageChange}>
+                        <option value="en">English</option>
+                        <option value="it">Italian</option>
+                    </select>
+                    <DisplayLanguage />
+                </LanguageContext.Provider>
+                {/* <GitHubUser username="GiovanniGiannitto"/> 
                 <GitHubUserList />
                 <HookCounter />
                 <HookForm />
-                <CarDetail initialData={{model:"Alfa Mito", year:2008, color:"blue"}}/>
+                <CarDetail initialData={{model:"Alfa Mito", year:2008, color:"blue"}}/>*/}
             </div>
         )
     }
