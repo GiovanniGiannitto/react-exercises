34855154326ec56a3a4d47be2e913ca8b6c15f53
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-06-09 15:02:45 +0200

    add-Root.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { FilteredList } from "./FilteredList";
+import { Route, Routes } from "react-router-dom";
+// import { FilteredList } from "./FilteredList";
 // import { CarDetail } from "./CarDetail";
 // import { ClickCounter } from "./ClickCounter";
 // import { ClickTracker } from "./ClickTracker";
-// import { Container } from "./Container";
+import { Container } from "./Container";
 // import { Counter } from "./Counter";
 // import { DisplayLanguage } from "./DisplayLanguage";
 // import { GitHubUser } from "./GitHubUser";
@@ -15,24 +16,24 @@ import { FilteredList } from "./FilteredList";
 // import { Sum } from "./Sum";
 // import { TodoList } from "./TodoList";
 // import { UncontrolledLogin } from "./UncontrolledLogin";
-// import { Welcome } from "./Welcome";
+import { Welcome } from "./Welcome";
 // import { HookCounter } from "./HookCounter"
 // import { HookForm } from "./HookForm";
 
 export class App extends React.Component {
-    state = {
-        language: "en"
-    }
+  state = {
+    language: "en",
+  };
 
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-    render() {
-        return (
-            <div>
-                {/* 
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        {/* 
                 <Hello />
                 <Welcome name="John" renderAge={true}/>
                 <Counter initialValue={2} incrementAmount={2} incrementInterval={10000} />
@@ -49,22 +50,26 @@ export class App extends React.Component {
                         )
                     }
                 }/>
-                <Container title="My prop is a title" />
                 <Sum /> */}
-                {/* <LanguageContext.Provider value={this.state.language}>
+        {/* <LanguageContext.Provider value={this.state.language}>
                     <select value={this.state.language} onChange={this.handleLanguageChange}>
                         <option value="en">English</option>
                         <option value="it">Italian</option>
                     </select>
                     <DisplayLanguage />
                 </LanguageContext.Provider> */}
-                {/* <GitHubUser username="GiovanniGiannitto"/> 
+        {/* <GitHubUser username="GiovanniGiannitto"/> 
                 <GitHubUserList />
                 <HookForm />
                 <CarDetail initialData={{model:"Alfa Mito", year:2008, color:"blue"}}/>
-                <HookCounter />*/}
-                <FilteredList />
-            </div>
-        )
-    }
+                <HookCounter />
+                <FilteredList />*/}
+        <Container title="My prop is a title">
+          <Routes>
+            <Route path="/" element={<Welcome name="Giovanni" />} />
+          </Routes>
+        </Container>
+      </div>
+    );
+  }
 }
